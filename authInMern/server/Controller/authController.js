8c6abed36860ecbfbbb2d2aecb69ac3b93e2b99a
Jesia23 <tahasinajesia@gmail.com>
2025-04-04 01:41:31 +0600

    Update authController.js

--- authInMern/server/Controller/authController.js
+++ authInMern/server/Controller/authController.js
@@ -1,3 +1,31 @@
 const User = require("../models/User");   //directory is still left
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
+//Controll login
+const login = async(req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        //find user using username
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json( {message: "User not Valid" } ); 
+        }
+        
+        //comparing passwords using hash passwords
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Password not Valid"} );
+        }
+
+        //create and assign token
+        const token = jwt.sign({ id:user._id }, process.env.JWT_SECRET, {expiredIn: "1h" });
+        res.json({token});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error"});
+    }
+};
+
+module.exports = { login };
