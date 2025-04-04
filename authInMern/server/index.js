682be52df89a4e62b5cad7bfd83cfb587011c2c5
Jesia23 <tahasinajesia@gmail.com>
2025-04-04 13:29:36 +0600

    Update index.js

--- authInMern/server/index.js
+++ authInMern/server/index.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors")
+const db = require("./db");         //Importing database connections
+const authRoutes = require('./routes/authRoutes');   
 const app = express();
-const cors = require("cors");
-const { copyFileSync } = require("fs");
-
 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+//connect to database
+db();
+
+//Use authenticationroutes
+app.use('/api/auth', authRoutes);   //Mount the auth Routes
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`)); 
