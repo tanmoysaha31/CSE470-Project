f2b338b9420c68ba31d48c81824cbb82e1b48242
DebajyotiHalder09 <dhhlderayon@gmail.com>
2025-04-14 11:37:35 +0600

    added dashboard, mongodb connection, feature page

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const { test,registerUser,loginUser,getProfile } = require('../controllers/authController');
+
+//middleware
+router.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    }),
+)
+
+router.get('/', test)
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+router.get('/profile', getProfile)
+module.exports = router
