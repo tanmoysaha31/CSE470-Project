1b67e7e7c3c2ab5065b112a746c1ef5bf4885409
Tanmoy Saha <tanmoysaha2003@gmail.com>
2025-04-30 20:29:02 +0600

    New Feature

---
+++ server/models/task.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const taskSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['event', 'task'],
+        required: true
+    },
+    start: {
+        type: Date,
+        required: true
+    },
+    end: {
+        type: Date,
+        required: true
+    },
+    description: String,
+    color: String,
+    allDay: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
+
