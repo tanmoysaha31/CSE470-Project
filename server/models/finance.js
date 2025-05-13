38fbe2e13eb50e438446e1bad29a113967f38d88
Jesia23 <tahasinajesia@gmail.com>
2025-05-13 23:39:39 +0600

    Create finance.js

---
+++ server/models/finance.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const financeSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['income', 'expense'],
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Finance', financeSchema);
